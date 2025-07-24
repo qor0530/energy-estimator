551a4d0cb1ca20e49649939364b48f7508e12548
JongHwa Paik <qorwhdghk12@gmail.com>
2025-07-24 12:23:53 +0900

    ✨ feat : 라벨 문제 해결

--- visualization/map_utils.py
+++ visualization/map_utils.py
@@ -10,6 +10,22 @@ SMP_DICT = {
     "제주": 123.88
 }
 
+# 등급 점수 매핑
+등급_점수표 = {
+    "매우 추천": 3,
+    "추천": 2,
+    "확인 필요": 1,
+    "비추천": 0
+}
+
+# 색상 매핑
+색상표 = {
+    "green": "green",
+    "orange": "orange",
+    "lightblue": "lightblue",
+    "gray": "gray"
+}
+
 # 주소에서 지역 구분 함수
 def check_region(address):
     if str(address).startswith("제주특별자치도"):
@@ -23,33 +39,28 @@ def calculate_revenue(row, colname):
     smp_price = SMP_DICT.get(region, 0)
     return row[colname] * smp_price
 
-# 색상 결정 로직 (선택된 발전 종류에 따라 동작)
-def 추천색상(등급_태양광, 등급_풍력, selected_sources):
+# 색상 결정 로직 (기준 변경 반영)
+def 추천색상(등급_태양광, 등급_풍력, 발전량_태양광, 발전량_풍력, selected_sources):
+    점수_태양 = 등급_점수표.get(등급_태양광, 0)
+    점수_풍력 = 등급_점수표.get(등급_풍력, 0)
+
     if selected_sources == ["태양광"]:
         return 추천색상_단일(등급_태양광)
     elif selected_sources == ["풍력"]:
         return 추천색상_단일(등급_풍력)
     else:
-        # 둘 다 같은 경우 -> 높은 등급 기준으로 색상
-        if 등급_태양광 == 등급_풍력:
-            return 추천색상_단일(등급_태양광)
+        # ✅ 등급 점수 기준 우선 비교
+        if 점수_태양 <= 1 and 점수_풍력 <= 1:
+            return "gray"
+        elif 점수_태양 >= 2 and 점수_풍력 >= 2 and 점수_태양 == 점수_풍력:
+            return "green"
+        elif 점수_태양 > 점수_풍력:
+            return "orange"
+        elif 점수_풍력 > 점수_태양:
+            return "lightblue"
+        else:
+            return "gray"
 
-        # 하나라도 "매우 추천" 또는 "추천"이 있으면 해당 색
-        우선순위 = {"매우 추천": 3, "추천": 2, "확인 필요": 1, "비추천": 0}
-        등급값 = {
-            "태양광": 우선순위.get(등급_태양광, 0),
-            "풍력": 우선순위.get(등급_풍력, 0)
-        }
-
-        if 등급값["태양광"] >= 2 or 등급값["풍력"] >= 2:
-            # 더 높은 등급 쪽으로 색상 결정
-            if 등급값["태양광"] >= 등급값["풍력"]:
-                return 추천색상_단일(등급_태양광)
-            else:
-                return 추천색상_단일(등급_풍력)
-
-        # 둘 다 낮은 경우 회색
-        return "gray"
 
 def 추천색상_단일(등급):
     return {
@@ -87,6 +98,8 @@ def create_site_map(df, selected_sources):
             background_color=추천색상(
                 row["태양광_추천등급"],
                 row["풍력_추천등급"],
+                row["태양광_연간_총_발전량(kWh)"],
+                row["풍력_연간_총_발전량(kWh)"],
                 selected_sources
             ),
             border_color="white",
@@ -106,75 +119,29 @@ def create_site_map(df, selected_sources):
 
     return m
 
-# 범례 생성 함수 (선택된 발전 종류에 따라 조정)
+# 범례 생성 함수 (기존 비교 기준)
 def get_map_legend(selected_sources):
-    if selected_sources == ["태양광"]:
-        legend_html = """
-        {% macro html(this, kwargs) %}
-        <div style="
-            position: fixed;
-            bottom: 50px;
-            left: 50px;
-            z-index: 9999;
-            background-color: white;
-            padding: 10px;
-            border: 2px solid grey;
-            font-size: 14px;
-            color: black;
-        ">
-        <b>📍 범례 (태양광 추천 기준)</b><br>
-        <span style='color:green'>●</span> 매우 추천<br>
-        <span style='color:orange'>●</span> 추천<br>
-        <span style='color:lightblue'>●</span> 확인 필요<br>
-        <span style='color:gray'>●</span> 비추천
-        </div>
-        {% endmacro %}
-        """
-    elif selected_sources == ["풍력"]:
-        legend_html = """
-        {% macro html(this, kwargs) %}
-        <div style="
-            position: fixed;
-            bottom: 50px;
-            left: 50px;
-            z-index: 9999;
-            background-color: white;
-            padding: 10px;
-            border: 2px solid grey;
-            font-size: 14px;
-            color: black;
-        ">
-        <b>📍 범례 (풍력 추천 기준)</b><br>
-        <span style='color:green'>●</span> 매우 추천<br>
-        <span style='color:orange'>●</span> 추천<br>
-        <span style='color:lightblue'>●</span> 확인 필요<br>
-        <span style='color:gray'>●</span> 비추천
-        </div>
-        {% endmacro %}
-        """
-    else:
-        legend_html = """
-        {% macro html(this, kwargs) %}
-        <div style="
-            position: fixed;
-            bottom: 50px;
-            left: 50px;
-            z-index: 9999;
-            background-color: white;
-            padding: 10px;
-            border: 2px solid grey;
-            font-size: 14px;
-            color: black;
-        ">
-        <b>📍 범례 (추천 비교 기준)</b><br>
-        <span style='color:green'>●</span> 동일 등급 또는 우수 등급 우세<br>
-        <span style='color:orange'>●</span> 태양광 우세<br>
-        <span style='color:lightblue'>●</span> 풍력 우세<br>
-        <span style='color:gray'>●</span> 기타 (모두 낮은 등급)
-        </div>
-        {% endmacro %}
-        """
-
+    legend_html = """
+    {% macro html(this, kwargs) %}
+    <div style="
+        position: fixed;
+        bottom: 50px;
+        left: 50px;
+        z-index: 9999;
+        background-color: white;
+        padding: 10px;
+        border: 2px solid grey;
+        font-size: 14px;
+        color: black;
+    ">
+    <b>📍 범례 (추천 비교 기준)</b><br>
+    <span style='color:green'>●</span> 동일 등급 또는 우수 등급 우세<br>
+    <span style='color:orange'>●</span> 태양광 우세<br>
+    <span style='color:lightblue'>●</span> 풍력 우세<br>
+    <span style='color:gray'>●</span> 기타 (모두 낮은 등급)
+    </div>
+    {% endmacro %}
+    """
     legend = MacroElement()
     legend._template = Template(legend_html)
     return legend
